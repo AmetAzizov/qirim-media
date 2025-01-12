f6dfa421eb26b52a18bf054a667ed6ca250bc4c3
AmetAzizov <azizov.amet@gmail.com>
2025-01-12 17:24:40 +0100

    12.01.25

--- server-cms/config/plugins.ts
+++ server-cms/config/plugins.ts
@@ -18,21 +18,3 @@ export default ({ env }) => ({
     },
   },
 });
-
-
-// export default ({ env }) => ({
-//   upload: {
-//     provider: 'aws-s3',
-//     providerOptions: {
-//       accessKeyId: env('AWS_ACCESS_KEY_ID'),
-//       secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-//       region: env('AWS_REGION'),
-//       params: {
-//         Bucket: env('S3_BUCKET'),
-//       },
-//       s3Options: {
-//         endpoint: env('S3_ENDPOINT'),
-//       },
-//     },
-//   },
-// });
